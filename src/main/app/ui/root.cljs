36837fc8a73a1470f8abd632b973074c763a9e19
Alex Eberts <alex.eberts@gmail.com>
2021-03-05 16:08:36 -0800

    Using :initial-state to populate UI

--- src/main/app/ui/root.cljs
+++ src/main/app/ui/root.cljs
@@ -1,6 +1,7 @@
 (ns app.ui.root
   (:require
     [app.model.session :as session]
+    [app.application :as app :refer [SPA]]
     [clojure.string :as str]
     [taoensso.timbre :as log]
     [com.fulcrologic.fulcro.dom.events :as evt]
@@ -20,15 +21,10 @@
     [com.fulcrologic.semantic-ui.icons :as i]))
 
 (defsc TodoItem [this {:item/keys [id label status]}]
-  {
-   :query         [:item/id :item/label :item/status]
-   :ident         :item/id
-   :initial-state (fn [params]
-                    {1 {:item/id 1 :item/label "Take out the trash" :item/status :not-done}
-                     2 {:item/id 2 :item/label "Paint the deck" :item/status :not-done}
-                     3 {:item/id 3 :item/label "Write TPS report" :item/status :not-done}
-                     4 {:item/id 4 :item/label "Make copies" :item/status :not-done}})
-   }
+  {:query         [:item/id :item/label :item/status]
+   :ident         [:item/id :item/id]
+   :initial-state (fn [{:item/keys [id label status]}]
+                    {:item/id id :item/label label :item/status status})}
   (comp/fragment
     (ui-list-item {:className "todo-item"}
       (ui-list-content {:className "todo-content-button" :floated "right" :verticalAlign "middle"}
@@ -40,35 +36,31 @@
 (def ui-todo-item (comp/factory TodoItem {:keyfn :item/id}))
 
 (defsc ListItem [this {:list/keys [id label]}]
-  {
-   :query         [:list/id :list/label]
-   :ident         :list/id
-   :initial-state (fn [params]
-                    {1 {:list/id 1 :list/label "Home" :list/items (comp/get-initial-state TodoItem)}
-                     2 {:list/id 2 :list/label "Work" :list/items (comp/get-initial-state TodoItem)}})
-   }
+  {:query         [:list/id :list/label]
+   :ident         [:list/id :list/id]
+   :initial-state (fn [{:list/keys [id label]}]
+                    {:list/id id :list/label label})}
   (comp/fragment
     (ui-menu-item {:name label :active false})))
 
 (def ui-listitem (comp/factory ListItem {:keyfn :list/id}))
 
-(defsc Root [this {:root/keys [lists items ui]}]
-  {
-   :query         [{:root/lists (comp/get-query ListItem)}
-                   {:root/items (comp/get-query TodoItem)}
-                   :root/ui {}]
+(defsc Root [this {:root/keys [lists items]}]
+  {:query         [{:root/lists (comp/get-query ListItem)}
+                   {:root/items (comp/get-query TodoItem)}]
    :initial-state (fn [params]
-                    {:root/lists (comp/get-initial-state ListItem)
-                     :root/items (comp/get-initial-state TodoItem)
-                     :root/ui    {}})
-   }
+                    {:root/lists [(comp/get-initial-state ListItem {:list/id 1 :list/label "Work"})
+                                  (comp/get-initial-state ListItem {:list/id 2 :list/label "Play"})]
+                     :root/items [(comp/get-initial-state TodoItem {:item/id 1 :item/label "Take out the trash" :item/status :not-done})
+                                  (comp/get-initial-state TodoItem {:item/id 2 :item/label "Paint the deck" :item/status :not-done})
+                                  (comp/get-initial-state TodoItem {:item/id 3 :item/label "Write TPS report" :item/status :not-done})
+                                  (comp/get-initial-state TodoItem {:item/id 4 :item/label "Make copies" :item/status :not-done})]})}
   (div :.ui.container.segment
     (div :.ui.grid
       ;; region
       #_(div :.row                                          ; debug info uncomment form see it in the UI
           (div :.sixteen.wide.mobile.sixteen.wide.computer.column
             (dom/h3 "Debug info:")
-            (p ":ui data " (str ui))
             (p ":lists data " (str lists))
             (p ":items data " (str items))))
       ;; endregion
@@ -84,8 +76,13 @@
             (ui-list {:verticalAlign "middle"}
               (map ui-todo-item items))))))))
 
-
 (comment
-  (merge/merge-component! app.application/SPA Root)
+
+  ;;Notice a couple of things about this step:
+  ;;- We're using the "lambda form" of :initial-state here. In the next step we'll use the more concise "template form"
+  ;;- In general, you should prefer the "template form" of :initial-state because it will do error checking for you
+  ;;- The components in this example are expecting a map of individual elements so we need to call comp/get-initial-state for each component instance
+  ;;- :initial-state should be a map of the props that your component expects (i.e. the props in the argument list)
+  ;;- For ListItem the props it expects are: :list/id and :list/label because that is what we're destructing in props
 
   )
