08dff2f329bc69be1991902869d174bfae2025c6
Alex Eberts <alex.eberts@gmail.com>
2021-03-19 12:16:30 -0700

    Refactored selected items and added mutations to toggle selected item.

--- src/main/app/server_components/middleware.clj
+++ src/main/app/server_components/middleware.clj
@@ -41,6 +41,8 @@
       [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"}]
       [:link {:href "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
               :rel  "stylesheet"}]
+      [:link {:href "/css/custom-site.css"
+              :rel  "stylesheet"}]
       [:link {:rel "shortcut icon" :href "data:image/x-icon;," :type "image/x-icon"}]
       [:script (str "var fulcro_network_csrf_token = '" csrf-token "';")]]
      [:body
--- src/main/app/ui/root.cljs
+++ src/main/app/ui/root.cljs
@@ -1,14 +1,16 @@
 (ns app.ui.root
   (:require
     [app.model.session :as session]
-    [app.application :as app :refer [SPA]]
+    [app.application :refer [SPA]]
     [clojure.string :as str]
     [taoensso.timbre :as log]
+    [com.fulcrologic.fulcro.application :as app]
     [com.fulcrologic.fulcro.dom.events :as evt]
     [com.fulcrologic.fulcro.dom :as dom :refer [div p a]]
     [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
     [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
     [com.fulcrologic.fulcro.algorithms.merge :as merge]
+    [com.fulcrologic.fulcro.algorithms.denormalize :as fdn]
     [com.fulcrologic.fulcro-css.css :as css]
     [com.fulcrologic.semantic-ui.collections.menu.ui-menu :refer [ui-menu]]
     [com.fulcrologic.semantic-ui.collections.menu.ui-menu-item :refer [ui-menu-item]]
@@ -22,7 +24,12 @@
 
 (defmutation update-selected-list [{:list/keys [id] :as params}]
   (action [{:keys [app state] :as env}]
-    (swap! state assoc-in [:root/ui :selected-list-id] id)))
+    (swap! state assoc-in [:ui/selected-list] {:list/id id})))
+
+(defmutation toggle-item-status [{:item/keys [id status] :as params}]
+  (action [{:keys [app state] :as env}]
+    (let [new-status ((fn [s] (if (= s :done) :not-started :done)) status)]
+      (swap! state update-in [:item/id id] assoc :item/status new-status))))
 
 (defsc TodoItem [this {:item/keys [id label status]}]
   {:query         [:item/id :item/label :item/status]
@@ -34,8 +41,11 @@
       (ui-list-content {:className "todo-content-button" :floated "right" :verticalAlign "middle"}
         (ui-button {:className "todo-button" :compact true} "Delete"))
       (ui-list-content {:floated "left"}
-        (ui-checkbox {:className "todo-checkbox"}))
-      (ui-list-content {:verticalAlign "middle"} label))))
+        (ui-checkbox {:className "todo-checkbox" :checked (if (= status :done) true false)
+                      :onClick   #(comp/transact! this [(toggle-item-status {:item/id id :item/status status})])}))
+      (ui-list-content {:className     (str "todo-label " (if (= status :done) "done"))
+                        :verticalAlign "middle"}
+        label))))
 
 (def ui-todo-item (comp/factory TodoItem {:keyfn :item/id}))
 
@@ -49,28 +59,31 @@
 
 (def ui-listitem (comp/factory ListItem {:keyfn :list/id}))
 
-(defsc Root [this {:root/keys [lists ui]}]
+(defsc Root [this {:root/keys [lists] :ui/keys [selected-list] :as props}]
   {:query         [{:root/lists (comp/get-query ListItem)}
-                   :root/ui]
-   :initial-state {:root/lists [{:id    1 :label "Work"
-                                 :items [{:id 1 :label "Take out the trash" :status :not-done}
-                                         {:id 2 :label "Paint the deck" :status :not-done}]}
-                                {:id    2 :label "Play"
-                                 :items [{:id 3 :label "Write TPS report" :status :not-done}
-                                         {:id 4 :label "Make copies" :status :not-done}]}]
-                   :root/ui    {:selected-list-id 1}}}
-  (let [selected-list (ui :selected-list-id)
-        selected-list-items (-> lists (get (- selected-list 1)) (:list/items))]
+                   :ui/selected-list]
+   :initial-state {:root/lists       [{:id    1 :label "Work"
+                                       :items [{:id 1 :label "Take out the trash" :status :not-started}
+                                               {:id 2 :label "Paint the deck" :status :done}]}
+                                      {:id    2 :label "Play"
+                                       :items [{:id 3 :label "Write TPS report" :status :done}
+                                               {:id 4 :label "Make copies" :status :not-started}]}]
+                   :ui/selected-list {:list/id 1}
+                   ;:ui/selected-todos {}
+                   }}
+  (let [selected-todos (some :list/items (map #(if (= (:list/id selected-list) (:list/id %)) %) lists))]
     (div :.ui.container.segment
       (div :.ui.grid
         ;; region
-        (div :.row                                          ; debug info - uncomment this form see it in the UI
-          (div :.sixteen.wide.mobile.sixteen.wide.computer.column
-            (dom/h3 "Debug info:")
-            #_(p ":lists data " (str lists))
-            #_(p ":ui data " (str ui))
-            (p "selected-list: " (str selected-list))
-            ))
+        #_(div :.row                                        ; debug info - uncomment this form see it in the UI
+            (div :.sixteen.wide.mobile.sixteen.wide.computer.column
+              (dom/h3 "Debug info:")
+              #_(p ":lists data " (str lists))
+              #_(p ":ui data " (str ui))
+              (p "lists: " (str lists))
+              (p "selected-list: " (str selected-list))
+              (p "selected-todos: " (str selected-todos))
+              ))
         ;; endregion
         (div :.row
           (div :.sixteen.wide.mobile.four.wide.computer.column
@@ -82,17 +95,23 @@
             (dom/h2 #js {:style #js {:marginBottom "25px"}} "Tasks")
             (div :.row
               (ui-list {:verticalAlign "middle"}
-                (map ui-todo-item selected-list-items)))))))))
-
-
+                (map ui-todo-item selected-todos)))))))))
 
 (comment
 
-  ;;Notice a couple of things about this step:
+  (def s (app/current-state SPA))
+
+  (fdn/db->tree (comp/get-query Root) (comp/get-initial-state Root {}) {})
+
+  (def root-state (comp/get-initial-state Root {}))
+
+  (fdn/db->tree [{:root/lists [:list/label]}] (comp/get-initial-state Root {}) {})
+
+  ;; What we did in this step:
+  ;; Added a mutation to update the item status when the checkbox is clicked
+  ;; Added a custom-site.css file and updated shadow-cljs.edn to watch "resources/public" directory
+  ;; Updated to the latest version of Fulcro
   ;;
-  ;;- We're using the more concise "template form" of :initial-state in the Root component
-  ;;- Since the template form of :initial-state only supports simple keywords (not namespaced keywords) we need to use the lambda form in ListItem and TodoItem to destructure the incoming params
-  ;;- In general, you should prefer the "template form" of :initial-state because it will do error checking for you
-  ;;- The template form of :initial-state auto-derives the joins from the query so we don't need (comp/get-initial-state ListItem) etc. here
+  ;; Next: Make the Lists component show which list is selected based on :ui/selected-list
 
   )
